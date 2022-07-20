2460b912b23288c9a0be5353bf2420310a012849
Guinbi <90347338+Guinbi@users.noreply.github.com>
2022-07-20 13:48:24 -0300

    Movimentação do personagem

---
+++ HNG.cxx
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <conio.h>
+#include <string>
+
+using namespace std;
+
+#define KEY_UP 119
+#define KEY_DOWN 115
+#define KEY_LEFT 97
+#define KEY_RIGHT 100
+#define KEY_X 120
+
+int player_pos_x = 0;
+int player_pos_y = 0;
+
+void input_key(){
+    
+    char key = getch();
+    int value = key;
+    
+    while(value != KEY_X){
+        
+        switch(getch()){
+            
+            case KEY_UP:
+                player_pos_y += 1;
+                cout << "norte - " << "(" << player_pos_x << "," << player_pos_y << ")" << "\n";
+                break;
+                
+            case KEY_DOWN:
+                player_pos_y -= 1;
+                cout << "sul - " << "(" << player_pos_x << "," << player_pos_y << ")" << "\n";
+                break;
+                
+            case KEY_LEFT:
+                player_pos_x -= 1;
+                cout << "oeste - " << "(" << player_pos_x << "," << player_pos_y << ")" << "\n";
+                break;
+                
+            case KEY_RIGHT:
+                player_pos_x += 1;
+                cout << "leste - " << "(" << player_pos_x << "," << player_pos_y << ")" << "\n";
+                break;
+        }
+    }
+}
+
+int main(){
+    input_key();
+    
+    system("pause");
+    return 0;
+}
